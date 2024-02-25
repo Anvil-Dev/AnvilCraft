d4a11346d8ee11b92e34c332f17a41cf9f670cf1
Gugle <administration@ztxy666.cn>
2024-02-25 16:31:56 +0800

    init

---
+++ src/main/java/dev/dubhe/anvilcraft/AnvilCraft.java
@@ -0,0 +1,10 @@
+package dev.dubhe.anvilcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AnvilCraft implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/dubhe/anvilcraft/client/AnvilCraftClient.java
@@ -0,0 +1,10 @@
+package dev.dubhe.anvilcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnvilCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
