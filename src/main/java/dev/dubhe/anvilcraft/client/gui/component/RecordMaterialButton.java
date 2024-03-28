19fb6f87cc1917cb81a2bfedfa8570dc7803f350
Gugle <administration@ztxy666.cn>
2024-03-28 14:41:51 +0800

    GUI按钮的工具提示

--- src/main/java/dev/dubhe/anvilcraft/client/gui/component/RecordMaterialButton.java
+++ src/main/java/dev/dubhe/anvilcraft/client/gui/component/RecordMaterialButton.java
@@ -3,24 +3,45 @@ package dev.dubhe.anvilcraft.client.gui.component;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.dubhe.anvilcraft.AnvilCraft;
 import lombok.Getter;
-import lombok.Setter;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 
-@Setter
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Getter
 public class RecordMaterialButton extends Button {
     private boolean record;
     private static final ResourceLocation YES = AnvilCraft.of("textures/gui/container/button_yes.png");
     private static final ResourceLocation NO = AnvilCraft.of("textures/gui/container/button_no.png");
-    private static final MutableComponent defaultMessage = Component.translatable("");
+    private static final MutableComponent defaultMessage = Component.translatable("screen.anvilcraft.button.record", Component.translatable("screen.anvilcraft.button.off"));
 
     public RecordMaterialButton(int x, int y, OnPress onPress, boolean record) {
         super(x, y, 16, 16, defaultMessage, onPress, (var) -> defaultMessage);
         this.record = record;
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        if (this.isHovered()) {
+            List<Component> components = new ArrayList<>() {{
+                this.add(getMessage());
+                this.add(Component.translatable("screen.anvilcraft.button.record.tooltip").withStyle(ChatFormatting.GRAY));
+            }};
+            guiGraphics.renderTooltip(Minecraft.getInstance().font, components, Optional.empty(), mouseX, mouseY);
+        }
+    }
+
+    public void setRecord(boolean record) {
+        this.record = record;
+        this.setMessage(Component.translatable("screen.anvilcraft.button.record", Component.translatable("screen.anvilcraft.button." + (this.isRecord() ? "on" : "off"))));
     }
 
     @Override
