12e297657264f5794b39380c1c8fd720d4cd5f29
Gugle <administration@ztxy666.cn>
2024-04-02 01:23:32 +0800

    诅咒金相关物品给玩家带来的debuff进行创造模式判断

--- src/main/java/dev/dubhe/anvilcraft/item/CuredItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/CuredItem.java
@@ -9,7 +9,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-public class CuredItem extends Item {
+public class CuredItem extends Item implements Cured {
     public CuredItem(Properties properties) {
         super(properties);
     }
@@ -17,18 +17,6 @@ public class CuredItem extends Item {
     @Override
     public void inventoryTick(ItemStack stack, Level level, Entity entity, int slotId, boolean isSelected) {
         super.inventoryTick(stack, level, entity, slotId, isSelected);
-        if (entity instanceof Player player) {
-            MobEffectInstance weakness = new MobEffectInstance(MobEffects.WEAKNESS, 200, 1, false, true);
-            MobEffectInstance slowness = new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 200, 1, false, true);
-            MobEffectInstance hungry = new MobEffectInstance(MobEffects.HUNGER, 200, 1, false, true);
-            player.addEffect((weakness));
-            int curedNumber = player.getInventory().countItem(ModItems.CURSED_GOLD_INGOT) + player.getInventory().countItem(ModItems.CURSED_GOLD_NUGGET) + player.getInventory().countItem(ModItems.CURSED_GOLD_BLOCK);
-            if (curedNumber>8){
-                player.addEffect((slowness));
-            }
-            if (curedNumber>64){
-                player.addEffect((hungry));
-            }
-        }
+        Cured.super.inventoryTick(stack, level, entity, slotId, isSelected);
     }
 }
