8df316f70fb76fbbf69c28833b33b9ec44eff9e7
wdfaESfaef <2622389575@qq.com>
2024-04-11 17:29:47 +0800

    修复方块透明度

--- fabric/src/main/java/dev/dubhe/anvilcraft/client/fabric/AnvilCraftClient.java
+++ fabric/src/main/java/dev/dubhe/anvilcraft/client/fabric/AnvilCraftClient.java
@@ -1,10 +1,14 @@
 package dev.dubhe.anvilcraft.client.fabric;
 
+import dev.dubhe.anvilcraft.init.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 
 public class AnvilCraftClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RESIN_BLOCK.get(), RenderType.translucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.AMBER_BLOCK.get(), RenderType.translucent());
     }
 }
