138e4ed073d7f9899f904cdbe5bd770a6a710e4a
Gugle <administration@ztxy666.cn>
2024-04-19 16:04:05 +0800

    电网范围网络同步

---
+++ common/src/main/java/dev/dubhe/anvilcraft/network/PowerGridRemovePack.java
@@ -0,0 +1,48 @@
+package dev.dubhe.anvilcraft.network;
+
+import dev.dubhe.anvilcraft.api.network.Packet;
+import dev.dubhe.anvilcraft.api.power.PowerGrid;
+import dev.dubhe.anvilcraft.client.renderer.PowerGridRenderer;
+import dev.dubhe.anvilcraft.init.ModNetworks;
+import lombok.Getter;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+public class PowerGridRemovePack implements Packet {
+    private final int grid;
+
+    /**
+     * 电网移除
+     */
+    public PowerGridRemovePack(@NotNull PowerGrid grid) {
+        this.grid = grid.hashCode();
+    }
+
+    /**
+     * @param buf 缓冲区
+     */
+    public PowerGridRemovePack(@NotNull FriendlyByteBuf buf) {
+        this.grid = buf.readInt();
+    }
+
+    @Override
+    public ResourceLocation getType() {
+        return ModNetworks.POWER_GRID_REMOVE;
+    }
+
+    @Override
+    public void encode(@NotNull FriendlyByteBuf buf) {
+        buf.writeInt(this.grid);
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void handler() {
+        Minecraft.getInstance().execute(() -> PowerGridRenderer.getGrids().remove(this.grid));
+    }
+}
