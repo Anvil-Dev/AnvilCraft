57ae631ed242528f4f69ec1b4a6b311f3540ec29
ZhuRuoLing <jkl-963@live.com>
2024-04-27 00:00:31 +0800

    添加皇家铁砧锤升级配方

--- common/src/main/java/dev/dubhe/anvilcraft/item/RoyalUpgradeTemplateItem.java
+++ common/src/main/java/dev/dubhe/anvilcraft/item/RoyalUpgradeTemplateItem.java
@@ -27,6 +27,10 @@ public class RoyalUpgradeTemplateItem extends SmithingTemplateItem {
             "screen.anvilcraft.smithing_template.royal_steel_upgrade_smithing_template.additions_slot_description");
     private static final ResourceLocation EMPTY_SLOT_PICKAXE = new ResourceLocation("item/empty_slot_pickaxe");
     private static final ResourceLocation EMPTY_SLOT_INGOT = new ResourceLocation("item/empty_slot_ingot");
+    private static final ResourceLocation EMPTY_SLOT_HAMMER = new ResourceLocation(
+            "anvilcraft",
+            "item/empty_slot_hammer"
+    );
     private static final ResourceLocation EMPTY_SLOT_BLOCK = new ResourceLocation(
             "anvilcraft",
             "item/empty_slot_block"
@@ -38,7 +42,7 @@ public class RoyalUpgradeTemplateItem extends SmithingTemplateItem {
                 UPGRADE,
                 UPGRADE_BASE_SLOT_DESCRIPTION,
             UPGRADE_ADDITIONS_SLOT_DESCRIPTION,
-                List.of(EMPTY_SLOT_PICKAXE),
+                List.of(EMPTY_SLOT_PICKAXE, EMPTY_SLOT_HAMMER),
                 List.of(EMPTY_SLOT_INGOT, EMPTY_SLOT_BLOCK)
         );
     }
