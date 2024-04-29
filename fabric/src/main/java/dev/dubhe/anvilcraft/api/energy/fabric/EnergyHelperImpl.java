d83dc5e1ae0a863f0083c77da48963429080b430
DancingSnow <1121149616@qq.com>
2024-04-29 23:57:08 +0800

    修复科技复兴api不存在时的崩溃

--- fabric/src/main/java/dev/dubhe/anvilcraft/api/energy/fabric/EnergyHelperImpl.java
+++ fabric/src/main/java/dev/dubhe/anvilcraft/api/energy/fabric/EnergyHelperImpl.java
@@ -1,22 +1,16 @@
 package dev.dubhe.anvilcraft.api.energy.fabric;
 
-import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
-import team.reborn.energy.api.EnergyStorage;
 
 public class EnergyHelperImpl {
-    @SuppressWarnings("UnstableApiUsage")
     public static void insertEnergy(Level level, BlockPos pos, Direction direction, int amount) {
-        EnergyStorage maybeStorage = EnergyStorage.SIDED.find(level, pos, direction);
-        if (maybeStorage != null) {
-            try (Transaction transaction = Transaction.openOuter()) {
-                long inserted = maybeStorage.insert(amount, transaction);
-                if (inserted > 0) {
-                    transaction.commit();
-                }
-            }
+        try {
+            Class.forName("team.reborn.energy.api.EnergyStorage");
+            EnergyInsert.insertEnergy(level, pos, direction, amount);
+        } catch (ClassNotFoundException ignore) {
+
         }
     }
 }
