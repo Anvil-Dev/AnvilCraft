e3b5d0522b4941fd88c325f0c37984405194e4d8
wdfaESfaef <2622389575@qq.com>
2024-05-01 19:07:19 +0800

    数值再调整

--- common/src/main/java/dev/dubhe/anvilcraft/event/PistonMoveBlockListener.java
+++ common/src/main/java/dev/dubhe/anvilcraft/event/PistonMoveBlockListener.java
@@ -6,7 +6,6 @@ import dev.dubhe.anvilcraft.block.entity.ChargeCollectorBlockEntity;
 import dev.dubhe.anvilcraft.init.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -19,48 +18,45 @@ import java.util.List;
 import java.util.Map;
 
 public class PistonMoveBlockListener {
-    private static final Map<Block, Double> CHANCES = new HashMap<>();
+    private static final Map<Block, Double> CHARGE_NUMS = new HashMap<>();
 
     static {
-        CHANCES.put(Blocks.COPPER_BLOCK, 1d / 4);
-        CHANCES.put(Blocks.EXPOSED_COPPER, 1d / 8);
-        CHANCES.put(Blocks.WEATHERED_COPPER, 1d / 16);
+        CHARGE_NUMS.put(Blocks.COPPER_BLOCK, 1d / 8);
+        CHARGE_NUMS.put(Blocks.EXPOSED_COPPER, 1d / 16);
+        CHARGE_NUMS.put(Blocks.WEATHERED_COPPER, 1d / 32);
     }
 
     /**
      * 活塞移动方块
      */
     public static void onPistonMoveBlocks(@NotNull Level level, @NotNull List<BlockPos> blocks) {
-        RandomSource random = level.random;
         for (BlockPos pos : blocks) {
             BlockState blockState = level.getBlockState(pos);
             if (!blockState.is(ModBlocks.MAGNET_BLOCK.get())) continue;
             if (blockState.getValue(MagnetBlock.LIT)) continue;
-            double c = getChance(level, pos);
-            double r = random.nextDouble();
-            if (c > 0 && r < c) {
+            double n = getChargeNum(level, pos);
+            if (n > 0) {
                 Collection<Map.Entry<Float, ChargeCollectorBlockEntity>> nearestChargeCollect =
                     ChargeCollectorManager.getNearestChargeCollect(pos);
                 for (var floatChargeCollectorBlockEntityEntry : nearestChargeCollect) {
                     ChargeCollectorBlockEntity blockEntity = floatChargeCollectorBlockEntityEntry.getValue();
                     if (ChargeCollectorManager.canCollect(blockEntity, pos)) {
-                        int unCharged = blockEntity.incomingCharge(1);
+                        double unCharged = blockEntity.incomingCharge(n);
                         if (unCharged == 0) {
                             break;
                         }
-
                     }
                 }
             }
         }
     }
 
-    private static Double getChance(Level level, BlockPos pos) {
+    private static Double getChargeNum(Level level, BlockPos pos) {
         double max = 0d;
         for (Direction face : Direction.values()) {
             Block block = level.getBlockState(pos.relative(face)).getBlock();
-            if (!CHANCES.containsKey(block)) continue;
-            max = max < CHANCES.get(block) ? CHANCES.get(block) : max;
+            if (!CHARGE_NUMS.containsKey(block)) continue;
+            max = max < CHARGE_NUMS.get(block) ? CHARGE_NUMS.get(block) : max;
         }
         return max;
     }
