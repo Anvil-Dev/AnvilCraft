876130e18f114d97f7b4f86475eb27aefee57524
ZhuRuoLing <jkl-963@live.com>
2024-05-10 11:16:58 +0800

    修复铁砧锤敲切石机破坏方块时错误破坏其他方块

---
+++ common/src/main/java/dev/dubhe/anvilcraft/api/IHasMultiBlock.java
@@ -0,0 +1,24 @@
+package dev.dubhe.anvilcraft.api;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * 多方块方块
+ */
+public interface IHasMultiBlock {
+
+    void onRemove(
+            @NotNull Level level,
+            @NotNull BlockPos pos,
+            @NotNull BlockState state
+    );
+
+    void onPlace(
+            @NotNull Level level,
+            @NotNull BlockPos pos,
+            @NotNull BlockState state
+    );
+}
