47d66d8622902952ecb56cccb6f01ea366def605
Gugle <administration@ztxy666.cn>
2024-06-08 19:04:53 +0800

    Update ItemTagLoader.java

--- common/src/main/java/dev/dubhe/anvilcraft/data/generator/tags/ItemTagLoader.java
+++ common/src/main/java/dev/dubhe/anvilcraft/data/generator/tags/ItemTagLoader.java
@@ -16,10 +16,10 @@ public class ItemTagLoader {
      */
     public static void init(@NotNull RegistrateTagsProvider<Item> provider) {
         provider.addTag(ModItemTags.PLATES).setReplace(false)
-                .add(Items.HEAVY_WEIGHTED_PRESSURE_PLATE)
-                .add(Items.LIGHT_WEIGHTED_PRESSURE_PLATE);
+            .add(Items.HEAVY_WEIGHTED_PRESSURE_PLATE)
+            .add(Items.LIGHT_WEIGHTED_PRESSURE_PLATE);
         provider.addTag(ModItemTags.IRON_PLATES).setReplace(false)
-                .add(Items.HEAVY_WEIGHTED_PRESSURE_PLATE);
+            .add(Items.HEAVY_WEIGHTED_PRESSURE_PLATE);
         provider.addTag(ModItemTags.GOLD_PLATES).setReplace(false)
             .add(Items.LIGHT_WEIGHTED_PRESSURE_PLATE);
         provider.addTag(ModItemTags.PLATES_FORGE).setReplace(false)
@@ -30,59 +30,59 @@ public class ItemTagLoader {
         provider.addTag(ModItemTags.GOLD_PLATES_FORGE).setReplace(false)
             .add(Items.LIGHT_WEIGHTED_PRESSURE_PLATE);
         provider.addTag(ModItemTags.ROYAL_STEEL_PICKAXE_BASE).setReplace(false)
-                .add(ModItems.AMETHYST_PICKAXE.get())
-                .add(Items.GOLDEN_PICKAXE)
-                .add(Items.IRON_PICKAXE)
-                .add(Items.DIAMOND_PICKAXE);
+            .add(ModItems.AMETHYST_PICKAXE.get())
+            .add(Items.GOLDEN_PICKAXE)
+            .add(Items.IRON_PICKAXE)
+            .add(Items.DIAMOND_PICKAXE);
         provider.addTag(ModItemTags.ROYAL_STEEL_AXE_BASE).setReplace(false)
-                .add(ModItems.AMETHYST_AXE.get())
-                .add(Items.GOLDEN_AXE)
-                .add(Items.IRON_AXE)
-                .add(Items.DIAMOND_AXE);
+            .add(ModItems.AMETHYST_AXE.get())
+            .add(Items.GOLDEN_AXE)
+            .add(Items.IRON_AXE)
+            .add(Items.DIAMOND_AXE);
         provider.addTag(ModItemTags.ROYAL_STEEL_HOE_BASE).setReplace(false)
-                .add(ModItems.AMETHYST_HOE.get())
-                .add(Items.GOLDEN_HOE)
-                .add(Items.IRON_HOE)
-                .add(Items.DIAMOND_HOE);
+            .add(ModItems.AMETHYST_HOE.get())
+            .add(Items.GOLDEN_HOE)
+            .add(Items.IRON_HOE)
+            .add(Items.DIAMOND_HOE);
         provider.addTag(ModItemTags.ROYAL_STEEL_SWORD_BASE).setReplace(false)
-                .add(ModItems.AMETHYST_SWORD.get())
-                .add(Items.GOLDEN_SWORD)
-                .add(Items.IRON_SWORD)
-                .add(Items.DIAMOND_SWORD);
+            .add(ModItems.AMETHYST_SWORD.get())
+            .add(Items.GOLDEN_SWORD)
+            .add(Items.IRON_SWORD)
+            .add(Items.DIAMOND_SWORD);
         provider.addTag(ModItemTags.ROYAL_STEEL_SHOVEL_BASE).setReplace(false)
-                .add(ModItems.AMETHYST_SHOVEL.get())
-                .add(Items.GOLDEN_SHOVEL)
-                .add(Items.IRON_SHOVEL)
-                .add(Items.DIAMOND_SHOVEL);
+            .add(ModItems.AMETHYST_SHOVEL.get())
+            .add(Items.GOLDEN_SHOVEL)
+            .add(Items.IRON_SHOVEL)
+            .add(Items.DIAMOND_SHOVEL);
         provider.addTag(ModItemTags.GEMS).setReplace(false)
-                .add(Items.EMERALD)
-                .add(ModItems.RUBY.get())
-                .add(ModItems.SAPPHIRE.get())
-                .add(ModItems.TOPAZ.get());
+            .add(Items.EMERALD)
+            .add(ModItems.RUBY.get())
+            .add(ModItems.SAPPHIRE.get())
+            .add(ModItems.TOPAZ.get());
         provider.addTag(ModItemTags.GEMS).setReplace(false)
-                .add(Items.EMERALD)
-                .add(ModItems.RUBY.get())
-                .add(ModItems.SAPPHIRE.get())
-                .add(ModItems.TOPAZ.get());
+            .add(Items.EMERALD)
+            .add(ModItems.RUBY.get())
+            .add(ModItems.SAPPHIRE.get())
+            .add(ModItems.TOPAZ.get());
         provider.addTag(ModItemTags.GEM_BLOCKS).setReplace(false)
-                .add(Items.EMERALD_BLOCK)
-                .add(ModBlocks.RUBY_BLOCK.asItem())
-                .add(ModBlocks.SAPPHIRE_BLOCK.asItem())
-                .add(ModBlocks.TOPAZ_BLOCK.asItem());
+            .add(Items.EMERALD_BLOCK)
+            .add(ModBlocks.RUBY_BLOCK.asItem())
+            .add(ModBlocks.SAPPHIRE_BLOCK.asItem())
+            .add(ModBlocks.TOPAZ_BLOCK.asItem());
         provider.addTag(ModItemTags.DEAD_TUBE).setReplace(false)
-                .add(Items.DEAD_BRAIN_CORAL)
-                .add(Items.DEAD_BUBBLE_CORAL)
-                .add(Items.DEAD_FIRE_CORAL)
-                .add(Items.DEAD_HORN_CORAL)
-                .add(Items.DEAD_TUBE_CORAL)
-                .add(Items.DEAD_TUBE_CORAL_FAN)
-                .add(Items.DEAD_BRAIN_CORAL_FAN)
-                .add(Items.DEAD_BUBBLE_CORAL_FAN)
-                .add(Items.DEAD_FIRE_CORAL_FAN)
-                .add(Items.DEAD_HORN_CORAL_FAN);
+            .add(Items.DEAD_BRAIN_CORAL)
+            .add(Items.DEAD_BUBBLE_CORAL)
+            .add(Items.DEAD_FIRE_CORAL)
+            .add(Items.DEAD_HORN_CORAL)
+            .add(Items.DEAD_TUBE_CORAL)
+            .add(Items.DEAD_TUBE_CORAL_FAN)
+            .add(Items.DEAD_BRAIN_CORAL_FAN)
+            .add(Items.DEAD_BUBBLE_CORAL_FAN)
+            .add(Items.DEAD_FIRE_CORAL_FAN)
+            .add(Items.DEAD_HORN_CORAL_FAN);
         provider.addTag(ModItemTags.VOID_RESISTANT).setReplace(false)
-                .add(ModItems.VOID_MATTER.get())
-                .add(ModBlocks.VOID_MATTER_BLOCK.asItem())
-                .add(ModBlocks.VOID_STONE.asItem());
+            .add(ModItems.VOID_MATTER.get())
+            .add(ModBlocks.VOID_MATTER_BLOCK.asItem())
+            .add(ModBlocks.VOID_STONE.asItem());
     }
 }
