51770366536a81a5dcd32c4599779216f7ae51a8
ZhuRuoLing <jkl-963@live.com>
2024-06-15 18:55:29 +0800

    定日镜添加进创造物品栏

--- common/src/main/java/dev/dubhe/anvilcraft/init/ModItemGroups.java
+++ common/src/main/java/dev/dubhe/anvilcraft/init/ModItemGroups.java
@@ -145,6 +145,7 @@ public class ModItemGroups {
                 entries.accept(ModBlocks.REMOTE_TRANSMISSION_POLE.asStack());
                 entries.accept(ModBlocks.INDUCTION_LIGHT.asStack());
                 entries.accept(ModBlocks.CHARGE_COLLECTOR.asStack());
+                entries.accept(ModBlocks.HELIOSTATS.asStack());
                 entries.accept(ModBlocks.LOAD_MONITOR.asStack());
                 entries.accept(ModBlocks.POWER_CONVERTER_SMALL.asStack());
                 entries.accept(ModBlocks.POWER_CONVERTER_MIDDLE.asStack());
