a7f2ba84499ecdaee9cedc346a242c1cf408e957
Gugle <administration@ztxy666.cn>
2024-06-25 12:05:49 +0800

    修改 Utils

--- fabric/src/main/java/dev/dubhe/anvilcraft/util/fabric/UtilsImpl.java
+++ fabric/src/main/java/dev/dubhe/anvilcraft/util/fabric/UtilsImpl.java
@@ -3,7 +3,7 @@ package dev.dubhe.anvilcraft.util.fabric;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class UtilsImpl {
-    public static boolean onlyJei() {
-        return FabricLoader.getInstance().isModLoaded("jei") && !FabricLoader.getInstance().isModLoaded("emi");
+    public static boolean isLoaded(String modid) {
+        return FabricLoader.getInstance().isModLoaded(modid);
     }
 }
