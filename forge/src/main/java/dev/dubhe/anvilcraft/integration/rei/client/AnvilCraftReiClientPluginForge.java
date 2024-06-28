4c54e52cf3ca0997621bb4834ee43fe8612e715b
dmzz-yyhyy <hk198580666@outlook.com>
2024-06-28 20:22:00 +0800

    修正forge包名

--- forge/src/main/java/dev/dubhe/anvilcraft/integration/rei/client/AnvilCraftReiClientPluginForge.java
+++
@@ -1,7 +0,0 @@
-package dev.dubhe.anvilcraft.integration.rei.client;
-
-import me.shedaniel.rei.forge.REIPluginClient;
-
-@REIPluginClient
-public class AnvilCraftReiClientPluginForge extends AnvilCraftReiClientPlugin {
-}
