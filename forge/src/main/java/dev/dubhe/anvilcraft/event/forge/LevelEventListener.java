dbc13aa377085a80250b298f592b5904d15029c2
dmzz-yyhyy <hk198580666@outlook.com>
2024-06-28 22:51:35 +0800

    eventListener修正

--- forge/src/main/java/dev/dubhe/anvilcraft/event/forge/LevelEventListener.java
+++ forge/src/main/java/dev/dubhe/anvilcraft/event/forge/LevelEventListener.java
@@ -7,6 +7,8 @@ import dev.dubhe.anvilcraft.api.recipe.AnvilRecipeManager;
 import dev.dubhe.anvilcraft.api.world.load.LevelLoadManager;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,6 +25,14 @@ public class LevelEventListener {
         if (event.getLevel() instanceof ServerLevel serverLevel)
             AnvilCraftBlockPlacer.anvilCraftBlockPlacer =
                 new AnvilCraftBlockPlacerFakePlayer(serverLevel);
+    }
+
+    /**
+     * 客户端世界加载事件
+     */
+    @OnlyIn(Dist.CLIENT)
+    @SubscribeEvent
+    public static void onClientLevelLoad(@NotNull LevelEvent.Load event) {
         if (event.getLevel() instanceof ClientLevel clientLevel) {
             AnvilRecipeManager.updateRecipes(clientLevel.getRecipeManager(), clientLevel.registryAccess());
         }
