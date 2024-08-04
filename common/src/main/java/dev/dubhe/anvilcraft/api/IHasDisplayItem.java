847469e4db19baa6f409abf3d00b31dccb3575da
ZhuRuoLing <jkl-963@live.com>
2024-08-05 05:14:03 +0800

    javadoc

--- common/src/main/java/dev/dubhe/anvilcraft/api/IHasDisplayItem.java
+++ common/src/main/java/dev/dubhe/anvilcraft/api/IHasDisplayItem.java
@@ -2,6 +2,9 @@ package dev.dubhe.anvilcraft.api;
 
 import net.minecraft.world.item.ItemStack;
 
+/**
+ * 带有展示物品的方块实体
+ */
 public interface IHasDisplayItem {
     void updateDisplayItem(ItemStack stack);
 }
