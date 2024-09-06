c84bf904dbe72dec48530b935b120b4f90fdbf15
ZhuRuoLing <jkl-963@live.com>
2024-09-07 00:15:10 +0800

    试图迁移到neoforge only

--- fabric/src/main/java/dev/dubhe/anvilcraft/util/fabric/UtilsImpl.java
+++
@@ -1,9 +0,0 @@
-package dev.dubhe.anvilcraft.util.fabric;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class UtilsImpl {
-    public static boolean isLoaded(String modid) {
-        return FabricLoader.getInstance().isModLoaded(modid);
-    }
-}
