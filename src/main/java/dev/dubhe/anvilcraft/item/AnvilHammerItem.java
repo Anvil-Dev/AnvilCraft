bc153f3522fe961f2efced313e3d9ad07f8e41e7
ZhuRuoLing <jkl-963@live.com>
2024-10-05 01:58:22 +0800

    为蟹钳添加attribute tooltip

--- src/main/java/dev/dubhe/anvilcraft/item/AnvilHammerItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/AnvilHammerItem.java
@@ -50,18 +50,22 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
     public AnvilHammerItem(Item.Properties properties) {
         super(properties);
         modifiers = ItemAttributeModifiers.builder()
-                .add(
-                        Attributes.ATTACK_DAMAGE,
-                        new AttributeModifier(
-                                BASE_ATTACK_DAMAGE_ID,
-                                getAttackDamageModifierAmount(),
-                                AttributeModifier.Operation.ADD_VALUE),
-                        EquipmentSlotGroup.MAINHAND)
-                .add(
-                        Attributes.ATTACK_SPEED,
-                        new AttributeModifier(BASE_ATTACK_SPEED_ID, -3F, AttributeModifier.Operation.ADD_VALUE),
-                        EquipmentSlotGroup.MAINHAND)
-                .build();
+            .add(
+                Attributes.ATTACK_DAMAGE,
+                new AttributeModifier(
+                    BASE_ATTACK_DAMAGE_ID,
+                    getAttackDamageModifierAmount(),
+                    AttributeModifier.Operation.ADD_VALUE),
+                EquipmentSlotGroup.MAINHAND
+            ).add(
+                Attributes.ATTACK_SPEED,
+                new AttributeModifier(
+                    BASE_ATTACK_SPEED_ID,
+                    -3F,
+                    AttributeModifier.Operation.ADD_VALUE
+                ),
+                EquipmentSlotGroup.MAINHAND
+            ).build();
     }
 
     protected float getAttackDamageModifierAmount() {
@@ -92,10 +96,10 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
     public static boolean ableToUseAnvilHammer(Level level, BlockPos blockPos, Player player) {
         BlockState state = level.getBlockState(blockPos);
         return state.getBlock() instanceof IHammerRemovable
-                || state.getBlock() instanceof IHammerChangeable
-                || state.is(ModBlockTags.HAMMER_REMOVABLE)
-                || state.is(ModBlockTags.HAMMER_CHANGEABLE)
-                || player.getOffhandItem().is(Items.FIREWORK_ROCKET);
+            || state.getBlock() instanceof IHammerChangeable
+            || state.is(ModBlockTags.HAMMER_REMOVABLE)
+            || state.is(ModBlockTags.HAMMER_CHANGEABLE)
+            || player.getOffhandItem().is(Items.FIREWORK_ROCKET);
     }
 
     private static void dropAnvil(Player player, Level level, BlockPos blockPos) {
@@ -103,7 +107,7 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
         if (System.currentTimeMillis() - lastDropAnvilTime <= 150) return;
         lastDropAnvilTime = System.currentTimeMillis();
         AnvilFallOnLandEvent event = new AnvilFallOnLandEvent(
-                level, blockPos.above(), new FallingBlockEntity(EntityType.FALLING_BLOCK, level), player.fallDistance);
+            level, blockPos.above(), new FallingBlockEntity(EntityType.FALLING_BLOCK, level), player.fallDistance);
         AnvilCraft.EVENT_BUS.post(event);
         level.playSound(null, blockPos, SoundEvents.ANVIL_LAND, SoundSource.BLOCKS, 1f, 1f);
         ItemStack itemStack = player.getItemInHand(player.getUsedItemHand());
@@ -116,7 +120,7 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
      * 右键方块
      */
     public static void useBlock(
-            @NotNull ServerPlayer player, BlockPos blockPos, @NotNull ServerLevel level, ItemStack anvilHammer) {
+        @NotNull ServerPlayer player, BlockPos blockPos, @NotNull ServerLevel level, ItemStack anvilHammer) {
         if (rocketJump(player, level, blockPos)) return;
         if (player.isShiftKeyDown()) {
             breakBlock(player, blockPos, level, anvilHammer);
@@ -138,15 +142,15 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
             serverPlayer.setDeltaMovement(0, power, 0);
             PacketDistributor.sendToPlayer(serverPlayer, new RocketJumpPacket(power));
             level.sendParticles(
-                    ParticleTypes.FIREWORK,
-                    serverPlayer.getX(),
-                    serverPlayer.getY(),
-                    serverPlayer.getZ(),
-                    20,
-                    0,
-                    0.5,
-                    0,
-                    0.05);
+                ParticleTypes.FIREWORK,
+                serverPlayer.getX(),
+                serverPlayer.getY(),
+                serverPlayer.getZ(),
+                20,
+                0,
+                0.5,
+                0,
+                0.05);
             level.playSound(null, blockPos, SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundSource.PLAYERS);
             return true;
         }
@@ -167,7 +171,7 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
 
     @Override
     public boolean canAttackBlock(
-            @NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player) {
+        @NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player) {
         return !player.isCreative();
     }
 
@@ -178,11 +182,11 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
 
     @Override
     public boolean mineBlock(
-            @NotNull ItemStack stack,
-            @NotNull Level level,
-            @NotNull BlockState state,
-            @NotNull BlockPos pos,
-            @NotNull LivingEntity miningEntity) {
+        @NotNull ItemStack stack,
+        @NotNull Level level,
+        @NotNull BlockState state,
+        @NotNull BlockPos pos,
+        @NotNull LivingEntity miningEntity) {
         if (state.getDestroySpeed(level, pos) != 0.0f) {
             stack.hurtAndBreak(2, miningEntity, LivingEntity.getSlotForHand(miningEntity.getUsedItemHand()));
         }
@@ -200,13 +204,13 @@ public class AnvilHammerItem extends Item implements Equipable, IEngineerGoggles
         target.hurt(target.level().damageSources().anvil(attacker), damageBonus);
         if (attacker.fallDistance >= 3) {
             attacker.level()
-                    .playSound(
-                            null,
-                            BlockPos.containing(attacker.position()),
-                            SoundEvents.ANVIL_LAND,
-                            SoundSource.BLOCKS,
-                            1f,
-                            attacker.fallDistance > 17 ? (float) 0.5 : 1 - attacker.fallDistance / 35);
+                .playSound(
+                    null,
+                    BlockPos.containing(attacker.position()),
+                    SoundEvents.ANVIL_LAND,
+                    SoundSource.BLOCKS,
+                    1f,
+                    attacker.fallDistance > 17 ? (float) 0.5 : 1 - attacker.fallDistance / 35);
         }
         return true;
     }
--- src/main/java/dev/dubhe/anvilcraft/item/CrabClawItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/CrabClawItem.java
@@ -5,28 +5,43 @@ import dev.dubhe.anvilcraft.api.entity.EntityHelper;
 import dev.dubhe.anvilcraft.api.entity.attribute.EntityReachAttribute;
 import dev.dubhe.anvilcraft.init.ModItems;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 
 import com.google.common.collect.Multimap;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 import java.util.function.Supplier;
 
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
 public class CrabClawItem extends Item {
 
-    public static final AttributeModifier rangeAttributeModifier =
-            new AttributeModifier(AnvilCraft.of("range_modifier"), 3, AttributeModifier.Operation.ADD_VALUE);
-
+    public static final AttributeModifier RANGE_ATTRIBUTE_MODIFIER = new AttributeModifier(
+        AnvilCraft.of("range_modifier"),
+        3,
+        AttributeModifier.Operation.ADD_VALUE
+    );
+    private static final Supplier<Multimap<Holder<Attribute>, AttributeModifier>> RANGE_MODIFIER_SUPPLIER =
+        EntityReachAttribute.getRangeModifierSupplier(RANGE_ATTRIBUTE_MODIFIER);
     public static final String CRAB_CLAW_MARKER = "crabClaw";
     public static final String DUAL_CRAB_CLAW_MARKER = "dualCrabClaw";
 
-    private static final Supplier<Multimap<Holder<Attribute>, AttributeModifier>> rangeModifier =
-            EntityReachAttribute.getRangeModifierSupplier(rangeAttributeModifier);
+
 
     public CrabClawItem(Properties properties) {
         super(properties);
@@ -46,12 +61,12 @@ public class CrabClawItem extends Item {
         boolean wasHoldingCrabClaw = customData.contains(CRAB_CLAW_MARKER);
         boolean wasHoldingDualCrabClaw = customData.contains(DUAL_CRAB_CLAW_MARKER);
         if (!holdingCrabClaw) {
-            player.getAttributes().removeAttributeModifiers(rangeModifier.get());
+            player.getAttributes().removeAttributeModifiers(RANGE_MODIFIER_SUPPLIER.get());
             if (wasHoldingCrabClaw) {
                 customData.remove(CRAB_CLAW_MARKER);
             }
         } else {
-            player.getAttributes().addTransientAttributeModifiers(rangeModifier.get());
+            player.getAttributes().addTransientAttributeModifiers(RANGE_MODIFIER_SUPPLIER.get());
             if (!wasHoldingDualCrabClaw) {
                 customData.putBoolean(CRAB_CLAW_MARKER, true);
             }
@@ -66,5 +81,17 @@ public class CrabClawItem extends Item {
                 customData.putBoolean(DUAL_CRAB_CLAW_MARKER, true);
             }
         }
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(CommonComponents.EMPTY);
+        tooltipComponents.add(Component.translatable("item.modifiers.hand").withStyle(ChatFormatting.GRAY));
+        tooltipComponents.add(Component.translatable(
+            "attribute.modifier.plus." + RANGE_ATTRIBUTE_MODIFIER.operation().id(),
+            ItemAttributeModifiers.ATTRIBUTE_MODIFIER_FORMAT.format(RANGE_ATTRIBUTE_MODIFIER.amount()),
+            Component.translatable(Attributes.BLOCK_INTERACTION_RANGE.value().getDescriptionId())
+        ).withStyle(Attributes.BLOCK_INTERACTION_RANGE.value().getStyle(true)));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
