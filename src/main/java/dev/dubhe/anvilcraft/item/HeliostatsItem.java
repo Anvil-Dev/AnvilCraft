2cf2c8f25d2ef865d2c752218a112240abadf267
ZhuRuoLing <jkl-963@live.com>
2024-10-14 23:00:41 +0800

    fixed #1140

--- src/main/java/dev/dubhe/anvilcraft/item/HeliostatsItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/HeliostatsItem.java
@@ -10,6 +10,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -19,10 +20,12 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -75,18 +78,18 @@ public class HeliostatsItem extends BlockItem {
 
     @Override
     protected boolean updateCustomBlockEntityTag(
-            @NotNull BlockPos pos,
-            @NotNull Level level,
-            @Nullable Player player,
-            @NotNull ItemStack stack,
-            @NotNull BlockState state) {
+        @NotNull BlockPos pos,
+        @NotNull Level level,
+        @Nullable Player player,
+        @NotNull ItemStack stack,
+        @NotNull BlockState state) {
         if (level.isClientSide) return false;
         if (!hasDataStored(stack)) {
             if (player != null) {
                 player.displayClientMessage(
-                        Component.translatable("block.anvilcraft.heliostats.placement_no_pos")
-                                .withStyle(ChatFormatting.RED),
-                        true);
+                    Component.translatable("block.anvilcraft.heliostats.placement_no_pos")
+                        .withStyle(ChatFormatting.RED),
+                    true);
             }
             return false;
         }
@@ -96,9 +99,9 @@ public class HeliostatsItem extends BlockItem {
         if (entity instanceof HeliostatsBlockEntity e) {
             if (!e.setIrritatePos(irritatePos) && player != null) {
                 player.displayClientMessage(
-                        Component.translatable("block.anvilcraft.heliostats.invalid_placement")
-                                .withStyle(ChatFormatting.RED),
-                        true);
+                    Component.translatable("block.anvilcraft.heliostats.invalid_placement")
+                        .withStyle(ChatFormatting.RED),
+                    true);
             }
             return true;
         }
@@ -107,19 +110,23 @@ public class HeliostatsItem extends BlockItem {
 
     @Override
     public void appendHoverText(
-            ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+        ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
         super.appendHoverText(stack, context, tooltipComponents, isAdvanced);
         if (hasDataStored(stack)) {
             BlockPos pos = getData(stack);
             tooltipComponents.add(Component.translatable("item.anvilcraft.heliostats.pos_set", pos.toShortString())
-                    .withStyle(Style.EMPTY.applyFormat(ChatFormatting.GRAY)));
+                .withStyle(Style.EMPTY.applyFormat(ChatFormatting.GRAY)));
         }
+    }
+
+    @Override
+    protected SoundEvent getPlaceSound(BlockState p_state, Level world, BlockPos pos, Player entity) {
+        return ModBlocks.HELIOSTATS.getDefaultState().getSoundType(world, pos, entity).getPlaceSound();
     }
 
     @Override
     public @NotNull InteractionResult useOn(@NotNull UseOnContext context) {
         Level level = context.getLevel();
-        if (level.isClientSide) return InteractionResult.PASS;
         if (context.getPlayer() != null && context.getPlayer().isShiftKeyDown()) {
             if (hasDataStored(context.getItemInHand())) {
                 deleteData(context.getItemInHand());
@@ -128,19 +135,19 @@ public class HeliostatsItem extends BlockItem {
         }
         BlockState blockState = level.getBlockState(context.getClickedPos());
         if (blockState.is(ModBlocks.TUNGSTEN_BLOCK.get())
-                || blockState.is(Blocks.NETHERITE_BLOCK)
-                || blockState.is(ModBlocks.HEATED_TUNGSTEN.get())
-                || blockState.is(ModBlocks.HEATED_NETHERITE.get())
-                || blockState.getBlock() instanceof RedhotMetalBlock) {
+            || blockState.is(Blocks.NETHERITE_BLOCK)
+            || blockState.is(ModBlocks.HEATED_TUNGSTEN.get())
+            || blockState.is(ModBlocks.HEATED_NETHERITE.get())
+            || blockState.getBlock() instanceof RedhotMetalBlock) {
             ItemStack stack = context.getItemInHand();
             if (hasDataStored(stack)) {
                 InteractionResult result = super.useOn(context);
                 if (result != InteractionResult.FAIL) {
                     level.playSound(
-                            context.getPlayer(),
-                            context.getClickedPos(),
-                            blockState.getSoundType().getPlaceSound(),
-                            SoundSource.BLOCKS);
+                        context.getPlayer(),
+                        context.getClickedPos(),
+                        blockState.getSoundType().getPlaceSound(),
+                        SoundSource.BLOCKS);
                 }
                 return result;
             } else {
@@ -152,10 +159,12 @@ public class HeliostatsItem extends BlockItem {
             InteractionResult result = super.useOn(context);
             if (result != InteractionResult.FAIL) {
                 level.playSound(
-                        context.getPlayer(),
-                        context.getClickedPos(),
-                        blockState.getSoundType().getPlaceSound(),
-                        SoundSource.BLOCKS);
+                    context.getPlayer(),
+                    context.getClickedPos(),
+                    blockState.getSoundType(context.getLevel(), context.getClickedPos(), context.getPlayer())
+                        .getPlaceSound(),
+                    SoundSource.BLOCKS
+                );
             }
             return result;
         }
@@ -163,7 +172,7 @@ public class HeliostatsItem extends BlockItem {
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(
-            Level level, @NotNull Player player, @NotNull InteractionHand usedHand) {
+        Level level, @NotNull Player player, @NotNull InteractionHand usedHand) {
         if (!level.isClientSide && player.isShiftKeyDown()) {
             ItemStack itemStack = player.getItemInHand(usedHand);
             if (hasDataStored(itemStack)) {
@@ -177,11 +186,11 @@ public class HeliostatsItem extends BlockItem {
     public record HeliostatsData(BlockPos pos) {
 
         public static final Codec<HeliostatsData> CODEC = RecordCodecBuilder.create(
-                ins -> ins.group(BlockPos.CODEC.fieldOf("pos").forGetter(HeliostatsData::pos))
-                        .apply(ins, HeliostatsData::new));
+            ins -> ins.group(BlockPos.CODEC.fieldOf("pos").forGetter(HeliostatsData::pos))
+                .apply(ins, HeliostatsData::new));
 
         public static final StreamCodec<ByteBuf, HeliostatsData> STREAM_CODEC =
-                StreamCodec.composite(BlockPos.STREAM_CODEC, HeliostatsData::pos, HeliostatsData::new);
+            StreamCodec.composite(BlockPos.STREAM_CODEC, HeliostatsData::pos, HeliostatsData::new);
 
         @Override
         public boolean equals(Object obj) {
