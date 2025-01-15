f6e2875c922bd8b42fc6b41ae51c5b06e624ca48
burin <ljx33071287@163.com>
2025-01-15 22:18:48 +0800

    remove unused import

--- src/main/java/dev/dubhe/anvilcraft/block/OilCauldronBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/OilCauldronBlock.java
@@ -16,7 +16,6 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.LayeredCauldronBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
