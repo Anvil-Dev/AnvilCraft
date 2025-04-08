da7ffa4c7989fb3ede3af0fbfe7ec094bdf40b57
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-04-09 05:55:26 +0800

    修改javadoc

--- src/main/java/dev/dubhe/anvilcraft/api/injection/UnimplementedException.java
+++ src/main/java/dev/dubhe/anvilcraft/api/injection/UnimplementedException.java
@@ -2,7 +2,7 @@ package dev.dubhe.anvilcraft.api.injection;
 
 /**
  * 当一个位于该软件包下的接口的方法未被Mixin等方法实现时抛出<br>
- * 位于该软件包下的接口的每个方法都应抛出该异常
+ * 位于该软件包下的接口的每个必须实现的方法都应抛出该异常
  */
 public class UnimplementedException extends RuntimeException {
     public UnimplementedException() {
