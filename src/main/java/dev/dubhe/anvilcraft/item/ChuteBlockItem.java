7d9e6bf99e5e81bc7924f7e6f6912fd58d863fac
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-04-23 06:17:08 +0800

    修改优先放置溜槽的条件
    更人性化了👍

--- src/main/java/dev/dubhe/anvilcraft/item/ChuteBlockItem.java
+++ src/main/java/dev/dubhe/anvilcraft/item/ChuteBlockItem.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.IItemHandler;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -21,6 +22,6 @@ public class ChuteBlockItem extends BlockItem {
     @Override
     public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
         BlockState state = context.getLevel().getBlockState(context.getClickedPos());
-        return state.getBlock() instanceof EntityBlock ? this.useOn(context) : super.onItemUseFirst(stack, context);
+        return state.getBlock() instanceof IItemHandler ? this.useOn(context) : super.onItemUseFirst(stack, context);
     }
 }
