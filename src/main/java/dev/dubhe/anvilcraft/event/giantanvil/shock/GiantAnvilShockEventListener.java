ab61cae820fb6d7a9104d6e2b9a8229230533c1d
ZhuRuoLing <jkl-963@live.com>
2025-05-06 19:46:34 +0800

    修改伤害行为

--- src/main/java/dev/dubhe/anvilcraft/event/giantanvil/shock/GiantAnvilShockEventListener.java
+++ src/main/java/dev/dubhe/anvilcraft/event/giantanvil/shock/GiantAnvilShockEventListener.java
@@ -126,8 +126,12 @@ public class GiantAnvilShockEventListener {
             );
             List<LivingEntity> e = it.unwrap().level().getEntitiesOfClass(LivingEntity.class, aabb);
             for (LivingEntity l : e) {
-                if (l.getItemBySlot(EquipmentSlot.FEET).is(Items.AIR)) {
-                    l.hurt(it.unwrap().level().damageSources().fall(), it.unwrap().fallDistance() * 2);
+                if (it.has(HURT_TYPE)) {
+                    l.hurt(it.getAttachment(HURT_TYPE, HurtType.class).damageSource(l.level()), it.unwrap().fallDistance() * 2);
+                } else {
+                    if (l.getItemBySlot(EquipmentSlot.FEET).is(Items.AIR)) {
+                        l.hurt(it.unwrap().level().damageSources().fall(), it.unwrap().fallDistance() * 2);
+                    }
                 }
             }
         });
