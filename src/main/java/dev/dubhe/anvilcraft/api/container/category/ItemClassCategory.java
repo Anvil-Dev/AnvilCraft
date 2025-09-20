010d6af1c7267d28661f21e240ac906ba0bf97f1
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2025-09-20 23:21:05 +0800

    修bug

--- src/main/java/dev/dubhe/anvilcraft/api/container/category/ItemClassCategory.java
+++ src/main/java/dev/dubhe/anvilcraft/api/container/category/ItemClassCategory.java
@@ -1,30 +1,35 @@
 package dev.dubhe.anvilcraft.api.container.category;
 
+import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.dubhe.anvilcraft.init.ModCategories;
-import dev.dubhe.anvilcraft.util.CodecUtil;
-import dev.dubhe.anvilcraft.util.Util;
+import lombok.SneakyThrows;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 
-public record ItemClassCategory(ItemStack icon, Component name, Class<? extends Item> clazz) implements ICategory {
-    public ItemClassCategory(ItemLike icon, String prefix, Class<? extends Item> clazz) {
-        this(icon.asItem().getDefaultInstance(), ICategory.constructName(prefix), clazz);
+public record ItemClassCategory(ItemStack icon, Component name, String clazz) implements ICategory {
+    private static ClassLoader loader;
+
+    private static ClassLoader getLoader() {
+        if (ItemClassCategory.loader != null) return ItemClassCategory.loader;
+        return ItemClassCategory.loader = ClassLoader.getSystemClassLoader();
     }
 
-    private ItemClassCategory(ItemStack icon, Class<?> itemClass, Component name) {
-        this(icon, name, Util.cast(itemClass));
+    public ItemClassCategory(ItemLike icon, String prefix, Class<? extends Item> clazz) {
+        this(icon.asItem().getDefaultInstance(), ICategory.constructName(prefix), clazz.getName());
     }
 
     @Override
+    @SneakyThrows
     public boolean test(ItemStack stack) {
-        return this.clazz.isInstance(stack.getItem());
+        return ItemClassCategory.getLoader().loadClass(this.clazz).isInstance(stack.getItem());
     }
 
     @Override
@@ -37,19 +42,20 @@ public record ItemClassCategory(ItemStack icon, Component name, Class<? extends 
             ItemStack.CODEC
                 .fieldOf("icon")
                 .forGetter(ItemClassCategory::icon),
-            CodecUtil.CLASS_CODEC
-                .forGetter(ItemClassCategory::clazz),
             ComponentSerialization.FLAT_CODEC
                 .fieldOf("name")
-                .forGetter(ItemClassCategory::name)
+                .forGetter(ItemClassCategory::name),
+            Codec.STRING
+                .fieldOf("class")
+                .forGetter(ItemClassCategory::clazz)
         ).apply(ins, ItemClassCategory::new));
         public static final StreamCodec<RegistryFriendlyByteBuf, ItemClassCategory> STREAM_CODEC = StreamCodec.composite(
             ItemStack.STREAM_CODEC,
             ItemClassCategory::icon,
-            CodecUtil.CLASS_STREAM_CODEC,
-            ItemClassCategory::clazz,
             ComponentSerialization.STREAM_CODEC,
             ItemClassCategory::name,
+            ByteBufCodecs.STRING_UTF8,
+            ItemClassCategory::clazz,
             ItemClassCategory::new
         );
 
