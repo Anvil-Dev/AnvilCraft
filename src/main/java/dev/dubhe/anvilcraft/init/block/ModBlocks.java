539bb73188165cfb41a8b905d047d4397dfb988d
Gugle <administration@ztxy666.cn>
2026-04-28 19:10:50 +0800

    style: Reformat @SuppressWarnings annotation in ModBlocks.java

--- src/main/java/dev/dubhe/anvilcraft/init/block/ModBlocks.java
+++ src/main/java/dev/dubhe/anvilcraft/init/block/ModBlocks.java
@@ -248,12 +248,10 @@ import static dev.dubhe.anvilcraft.AnvilCraft.of;
 import static dev.dubhe.anvilcraft.api.power.IPowerComponent.OVERLOAD;
 import static dev.dubhe.anvilcraft.api.power.IPowerComponent.SWITCH;
 
-@SuppressWarnings(
-    {
-        "unused",
-        "CodeBlock2Expr"
-    }
-)
+@SuppressWarnings({
+    "unused",
+    "CodeBlock2Expr"
+})
 public class ModBlocks {
     static {
         REGISTRUM.defaultCreativeTab(ModItemGroups.ANVILCRAFT_FUNCTION_BLOCK.getKey());
