04420a773127fb11722f1f3db49ac7e9ba95acad
TBPig <147127248+TBPig@users.noreply.github.com>
2026-05-11 19:43:55 +0800

    fix(ageratum): 修复copilot提到的问题

--- src/main/java/dev/dubhe/anvilcraft/event/GuideEventListener.java
+++ src/main/java/dev/dubhe/anvilcraft/event/GuideEventListener.java
@@ -1,17 +1,17 @@
 package dev.dubhe.anvilcraft.event;
 
 import dev.anvilcraft.resource.ageratum.Ageratum;
-import dev.anvilcraft.resource.ageratum.client.constants.AgeratumConstants;
 import dev.dubhe.anvilcraft.AnvilCraft;
 import dev.dubhe.anvilcraft.api.event.CheckIntegrationLoadedEvent;
 import dev.dubhe.anvilcraft.api.event.GuideBookEvent;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber(modid = AnvilCraft.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = AnvilCraft.MOD_ID)
 public class GuideEventListener {
+    public static final String INDEX_FILE = "index";
+
     @SubscribeEvent
     public static void onHasGuide(GuideBookEvent.HasGuideBookEvent event) {
         event.hasGuideBook();
@@ -26,7 +26,7 @@ public class GuideEventListener {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onOpenGuide(GuideBookEvent.OpenGuideBookEvent event) {
-        Ageratum.openGuide(event.getPlayer(), AnvilCraft.of(AgeratumConstants.Guide.INDEX_FILE));
+        Ageratum.openGuide(event.getPlayer(), AnvilCraft.of(INDEX_FILE));
         event.setCanceled(true);
     }
 }
--- src/main/resources/assets/anvilcraft/ageratum/zh_cn/002_material/213_multiple_template.md
+++
@@ -1,9 +0,0 @@
----
-navigation:
-  title: "§6多合一锻造模板"
-  icon: "anvilcraft:four_to_one_smithing_template"
-items:
-  - anvilcraft:two_to_one_smithing_template
-  - anvilcraft:four_to_one_smithing_template
-  - anvilcraft:eight_to_one_smithing_template
----
