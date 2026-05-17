a24ce8b8140419ee66010a2dd762bfb77c2cd7e5
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2026-05-17 17:24:45 +0800

    refactor: remove unnecessary @SuppressWarnings annotations and improve method parameter names

--- src/main/java/dev/dubhe/anvilcraft/event/PlayerTickEventHandler.java
+++ src/main/java/dev/dubhe/anvilcraft/event/PlayerTickEventHandler.java
@@ -7,7 +7,6 @@ import dev.dubhe.anvilcraft.item.property.component.Eternal;
 import dev.dubhe.anvilcraft.item.property.component.Ferocious;
 import dev.dubhe.anvilcraft.item.property.component.Merciless;
 import dev.dubhe.anvilcraft.item.property.component.MultiphaseRef;
-import dev.dubhe.anvilcraft.item.utility.CrabClawItem;
 import dev.dubhe.anvilcraft.item.weapon.SpectralWeaponLauncherItem;
 import dev.dubhe.anvilcraft.util.PlayerUtil;
 import net.minecraft.server.level.ServerPlayer;
