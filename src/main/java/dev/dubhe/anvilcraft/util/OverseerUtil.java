947182c0530ee4adad5b241c6c4402f8c6433e4c
cecilia <me@shigure.link>
2026-05-27 12:14:38 +0800

    Add `overseer` command

---
+++ src/main/java/dev/dubhe/anvilcraft/util/OverseerUtil.java
@@ -0,0 +1,41 @@
+package dev.dubhe.anvilcraft.util;
+
+import dev.dubhe.anvilcraft.block.OverseerBlock;
+import dev.dubhe.anvilcraft.block.entity.OverseerBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OverseerUtil {
+    private static final Map<ResourceKey<Level>, Set<BlockPos>> placedOverseers = new ConcurrentHashMap<>();
+
+    public static void OnLoadOverseer(Level level, OverseerBlockEntity overseerBlockEntity) {
+        if (!shouldTrack(level, overseerBlockEntity)) return;
+        placedOverseers
+            .computeIfAbsent(level.dimension(), dim -> new HashSet<>())
+            .add(overseerBlockEntity.getBlockPos());
+    }
+
+    public static void OnUnloadOverseer(Level level, OverseerBlockEntity overseerBlockEntity) {
+        if (!shouldTrack(level, overseerBlockEntity)) return;
+        placedOverseers
+            .getOrDefault(level.dimension(), Set.of())
+            .remove(overseerBlockEntity.getBlockPos());
+    }
+    public static Set<BlockPos> getPlacedOverseers(ResourceKey<Level> dimension) {
+        return OverseerUtil.placedOverseers.getOrDefault(dimension, Set.of());
+    }
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    private static boolean shouldTrack(Level level, OverseerBlockEntity overseerBlockEntity) {
+        if (level.isClientSide) return false;
+        BlockState state = overseerBlockEntity.getBlockState();
+        return ((OverseerBlock)state.getBlock()).isMainPart(state);
+    }
+}
