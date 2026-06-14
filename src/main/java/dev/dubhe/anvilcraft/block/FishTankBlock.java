44d881cdf729205496ca2495b9c0d64a76253ee7
TBPig <147127248+TBPig@users.noreply.github.com>
2026-06-14 20:40:35 +0800

    fix(fish_tank): 修复鱼缸吞火把、灵魂火把、营火、灵魂营火(#3649)

--- src/main/java/dev/dubhe/anvilcraft/block/FishTankBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/FishTankBlock.java
@@ -164,8 +164,10 @@ public class FishTankBlock extends Block implements IMoveableEntityBlock, Hammer
             return;
         }
         if (itemEntity.getItem().is(ModItemTags.FIRE_STARTER)) {
-            this.tryIgnite(level, pos);
-            itemEntity.getItem().setCount(itemEntity.getItem().getCount() - 1);
+            boolean result = this.tryIgnite(level, pos);
+            if (result) {
+                itemEntity.getItem().setCount(itemEntity.getItem().getCount() - 1);
+            }
         } else if (itemEntity.getItem().is(ModItemTags.UNBROKEN_FIRE_STARTER)) {
             this.tryIgnite(level, pos);
         }
