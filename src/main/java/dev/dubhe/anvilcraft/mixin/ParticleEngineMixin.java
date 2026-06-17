1fccf41f4f528c25da30e56679e59c9d2139eb1f
YocyCraft <hanhengrui@163.com>
2026-06-17 19:45:57 +0800

    chore(style): add comments for ParticleEngineMixin

--- src/main/java/dev/dubhe/anvilcraft/mixin/ParticleEngineMixin.java
+++ src/main/java/dev/dubhe/anvilcraft/mixin/ParticleEngineMixin.java
@@ -16,8 +16,13 @@ abstract class ParticleEngineMixin {
     @Shadow
     protected ClientLevel level;
 
+    /**
+        由于为具有空碰撞箱的加速环、偏转环中间部分增加了玩家持有相应物品时可互动的功能，该方法会对具有空形状的方块调用
+        但是后续的 {@code net.minecraft.world.phys.shapes.VoxelShape#bounds()} 方法会抛出异常
+        因此为避免崩溃，必须在这之前取消方法调用
+     */
     @Inject(method = "addBlockHitEffects", at = @At("HEAD"), cancellable = true)
-    private void damage(BlockPos pos, BlockHitResult target, CallbackInfo ci) {
+    private void cancelHitEffectForEmptyBlock(BlockPos pos, BlockHitResult target, CallbackInfo ci) {
         if (this.level.getBlockState(pos).getShape(this.level, pos).isEmpty()) {
             ci.cancel();
         }
