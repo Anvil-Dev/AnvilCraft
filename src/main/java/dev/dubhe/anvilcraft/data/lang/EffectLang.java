b218ad40980f09cac2162e283ce021797e50ce2c
burin <ljx33071287@163.com>
2026-06-25 21:25:21 +0800

    fix: totem of rage (#3854)

---
+++ src/main/java/dev/dubhe/anvilcraft/data/lang/EffectLang.java
@@ -0,0 +1,10 @@
+package dev.dubhe.anvilcraft.data.lang;
+
+import dev.anvilcraft.lib.v2.registrum.providers.RegistrumLangProvider;
+
+public class EffectLang {
+    public static void init(RegistrumLangProvider provider) {
+        provider.add("effect.anvilcraft.rage", "Rage");
+        provider.add("effect.anvilcraft.invulnerable", "Invulnerable");
+    }
+}
