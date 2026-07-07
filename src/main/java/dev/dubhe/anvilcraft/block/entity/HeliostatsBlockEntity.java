b5e684cb03798594bf61dec380ada0535e15c827
NightFish <101402767+dmzz-yyhyy@users.noreply.github.com>
2026-07-07 19:47:20 +0800

    修复定日镜太阳高度角

--- src/main/java/dev/dubhe/anvilcraft/block/entity/HeliostatsBlockEntity.java
+++ src/main/java/dev/dubhe/anvilcraft/block/entity/HeliostatsBlockEntity.java
@@ -138,9 +138,14 @@ public class HeliostatsBlockEntity extends BlockEntity {
             return WorkResult.OBSCURED;
         }
         double sunAngle = level.getSunAngle(1);
+        sunAngle = sunAngle - 270;
+        if (sunAngle < 0) {
+            sunAngle = sunAngle + 360;
+        }
         sunAngle = sunAngle <= Math.PI / 2 * 3 ? sunAngle + Math.PI / 2 : sunAngle - Math.PI / 2 * 3;
         if (sunAngle > Math.PI) return WorkResult.NO_SUN;
         Vector3f sunVector3f = new Vector3f((float) Math.cos(sunAngle), (float) Math.sin(sunAngle), 0).normalize();
+        sunAngle = Math.toRadians(sunAngle);
         irritateVector3f = new Vector3f(
             (float) (irritateVec3.x - getBlockPos().getX()),
             (float) (irritateVec3.y - getBlockPos().getY()),
