e9219b77c25752cb06369dc167da5f48afae5a9a
Fff <a181084170@gmail.com>
2026-07-19 01:47:03 +0800

    修红石导线

--- src/main/java/dev/dubhe/anvilcraft/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/dev/dubhe/anvilcraft/mixin/RedstoneWireBlockMixin.java
@@ -1,14 +1,20 @@
 package dev.dubhe.anvilcraft.mixin;
 
 import dev.dubhe.anvilcraft.api.injection.tooltip.ITooltipProviderExtension;
+import dev.dubhe.anvilcraft.block.RedstoneWireBlock;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +31,12 @@ public abstract class RedstoneWireBlockMixin implements ITooltipProviderExtensio
         lines.add(Component.translatable("tooltip.anvilcraft.redstone.title").withStyle(ChatFormatting.BLUE));
         lines.add(Component.translatable("tooltip.anvilcraft.redstone.power", state.getValue(POWER)).withStyle(ChatFormatting.GRAY));
         return lines;
+    }
+
+    @Inject(method = "calculateTargetStrength", at = @At("RETURN"), cancellable = true)
+    private void anvilcraft$includeUpwardWireSignal(
+        Level level, BlockPos pos, CallbackInfoReturnable<Integer> callback
+    ) {
+        callback.setReturnValue(Math.max(callback.getReturnValue(), RedstoneWireBlock.getUpwardDustSignal(level, pos)));
     }
 }
