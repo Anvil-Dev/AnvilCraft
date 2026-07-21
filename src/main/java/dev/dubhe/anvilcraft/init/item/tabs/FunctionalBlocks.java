d390755b34da0503b62baa9942e4956b7c850149
Fff <a181084170@gmail.com>
2026-07-21 13:47:42 +0800

    调创造物品栏位置

--- src/main/java/dev/dubhe/anvilcraft/init/item/tabs/FunctionalBlocks.java
+++ src/main/java/dev/dubhe/anvilcraft/init/item/tabs/FunctionalBlocks.java
@@ -40,8 +40,8 @@ public class FunctionalBlocks extends DisplayItemsGenerator {
         this.plain(ModBlocks.LARGE_CAULDRON); // 大型炼药锅
         this.plain(ModBlocks.FISH_TANK); // 鱼缸
         this.plain(ModBlocks.MENGER_SPONGE); // 门格海绵
+        this.plain(ModItems.FLUID_TANK_MINECART); // 流体储罐矿车
         this.plain(ModBlocks.FLUID_TANK); // 流体储罐
-        this.plain(ModItems.FLUID_TANK_MINECART);
         this.plain(ModBlocks.LARGE_FLUID_TANK); // 大型流体储罐
         this.plain(ModBlocks.DRAIN); // 排水口
         this.plain(ModItems.PIPE); // 管道
