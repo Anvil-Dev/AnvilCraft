ed80e6c949dfb877a5a9d4c745b59d544248ec10
QiuShui1012 <qiushui1012@foxmail.com>
2026-08-01 02:11:36 +0800

    fix: fix *valid* bug

--- src/main/java/dev/dubhe/anvilcraft/block/utility/redstone/PulseGeneratorBlock.java
+++ src/main/java/dev/dubhe/anvilcraft/block/utility/redstone/PulseGeneratorBlock.java
@@ -1,7 +1,5 @@
 package dev.dubhe.anvilcraft.block.utility.redstone;
 
-
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import com.mojang.serialization.MapCodec;
 import dev.anvilcraft.lib.v2.piston.IMoveableEntityBlock;
 import dev.dubhe.anvilcraft.api.hammer.IHammerChangeable;
@@ -28,6 +26,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -382,4 +381,3 @@ public class PulseGeneratorBlock extends HorizontalDirectionalBlock implements I
         be.setChanged();
     }
 }
-
